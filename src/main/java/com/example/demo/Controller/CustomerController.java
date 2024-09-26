709f0c09d7bd4e0c6730eed50fcad5ce8b211309
jamesWalkr <walkerjames9981@gmail.com>
2024-09-26 19:27:18 -0400

    added functionality to write database call to csv file

--- src/main/java/com/example/demo/Controller/CustomerController.java
+++ src/main/java/com/example/demo/Controller/CustomerController.java
@@ -4,8 +4,16 @@ import com.example.demo.models.Customer;
 import com.example.demo.models.Employee;
 import com.example.demo.repositories.CustomerRepo;
 import com.example.demo.repositories.EmployeeRepo;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,6 +42,23 @@ public class CustomerController {
         List<Employee> allEmployees = employeeRepo.findAll();
 
         return allEmployees;
+
+    }
+
+    @GetMapping("/csv")
+    public void exportCsv(HttpServletResponse response) throws CsvRequiredFieldEmptyException, CsvDataTypeMismatchException, IOException {
+        String fileName = "employee_data.csv";
+
+        response.setContentType("text/csv");
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename\"" + fileName +"");
+
+        StatefulBeanToCsv<Employee> writer = new StatefulBeanToCsvBuilder<Employee>(response.getWriter())
+                .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
+                .withOrderedResults(true)
+                .build();
+
+        writer.write(employeeRepo.findAll());
+
 
     }
 
